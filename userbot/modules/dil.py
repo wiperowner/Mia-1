3e5f8fbd400b0374ea6c0249546267f4d550ea62
Mehmet <86586138+wiperowner@users.noreply.github.com>
2021-08-29 10:34:17 +0300

    json değişecek

--- userbot/modules/dil.py
+++ userbot/modules/dil.py
@@ -1,11 +1,3 @@
-# Copyright (C) 2020 Yusuf Usta.
-#
-# Licensed under the  GPL-3.0 License;
-# you may not use this file except in compliance with the License.
-#
-
-# SiriUserBot - ErdemBey - Berceste - Midy
-
 from userbot.cmdhelp import CmdHelp
 from userbot import PLUGIN_CHANNEL_ID, CMD_HELP
 from userbot.events import register
