cd67d314733ae7aed4e604f76a9ec262eb56db5a
Mehmet <86586138+wiperowner@users.noreply.github.com>
2021-08-29 14:57:12 +0300

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,11 +1,4 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-
-# Thanks github.com/spechide for creating inline bot support.
-# SiriUserBot - Berceste
+# MiaUserBot - Mehmet
 """ UserBot hazırlanışı. """
 
 import os, sys, time, heroku3
