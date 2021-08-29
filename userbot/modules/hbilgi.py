c60b719c825ddba250ed591021e49107747a98d8
Mehmet <86586138+wiperowner@users.noreply.github.com>
2021-08-29 10:38:53 +0300

    hesap bilgi

--- userbot/modules/hbilgi.py
+++ userbot/modules/hbilgi.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 @register(outgoing=True, pattern=r"^.hbilgi(?: |$)(.*)") 
 async def hesapstat(event: NewMessage.Event) -> None:  # pylint: disable = R0912, R0914, R0915
     """Istatistikler için bir komut"""
-    waiting_message = await event.edit('`Siri Hesap Istatistikleri toplarken biraz bekle.`')
+    waiting_message = await event.edit('`Mia Hesap Istatistikleri buluyor...`')
     start_time = time.time()
     private_chats = 0
     bots = 0
