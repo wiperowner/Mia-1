07e1a74444d2ab6049713e79fd99fe1c07683efc
Mehmet <86586138+wiperowner@users.noreply.github.com>
2021-08-29 10:30:43 +0300

    Update auto.py

--- userbot/modules/auto.py
+++ userbot/modules/auto.py
@@ -1,13 +1,3 @@
-# Copyright (C) 2020 Yusuf Usta.
-#
-# Licensed under the  GPL-3.0 License;
-# you may not use this file except in compliance with the License.
-#
-
-# SiriUserBot - Berceste
-
-
-
 import asyncio
 import time
 from telethon.tl import functions
