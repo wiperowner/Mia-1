4d7512e8a04e549125dfc653988016aa8678484c
Mehmet <86586138+wiperowner@users.noreply.github.com>
2021-08-29 11:51:37 +0300

    miajson

--- userbot/language.py
+++ userbot/language.py
@@ -1,11 +1,3 @@
-# Copyright (C) 2020 Yusuf Usta.
-#
-# Licensed under the GPL-3.0 License;
-# you may not use this file except in compliance with the License.
-#
-
-# SiriUserBot - Berceste
-
 from . import LANGUAGE, LOGS, bot, PLUGIN_CHANNEL_ID
 from json import loads, JSONDecodeError
 from os import path, remove
@@ -16,7 +8,7 @@ LOGS.info("Dil dosyası yükleniyor...")
 LANGUAGE_JSON = None
 
 for dil in bot.iter_messages(pchannel, filter=InputMessagesFilterDocument):
-    if ((len(dil.file.name.split(".")) >= 2) and (dil.file.name.split(".")[1] == "sirijson")):
+    if ((len(dil.file.name.split(".")) >= 2) and (dil.file.name.split(".")[1] == "miajson")):
         if path.isfile(f"./userbot/language/{dil.file.name}"):
             try:
                 LANGUAGE_JSON = loads(open(f"./userbot/language/{dil.file.name}", "r").read())
@@ -24,9 +16,9 @@ for dil in bot.iter_messages(pchannel, filter=InputMessagesFilterDocument):
                 dil.delete()
                 remove(f"./userbot/language/{dil.file.name}")
 
-                if path.isfile("./userbot/language/DEFAULT.sirijson"):
+                if path.isfile("./userbot/language/DEFAULT.miajson"):
                     LOGS.warn("Varsayılan dil dosyası kullanılıyor...")
-                    LANGUAGE_JSON = loads(open(f"./userbot/language/DEFAULT.sirijson", "r").read())
+                    LANGUAGE_JSON = loads(open(f"./userbot/language/DEFAULT.miajson", "r").read())
                 else:
                     raise Exception("Your language file is invalid")
         else:
@@ -35,23 +27,23 @@ for dil in bot.iter_messages(pchannel, filter=InputMessagesFilterDocument):
                 LANGUAGE_JSON = loads(open(DOSYA, "r").read())
             except JSONDecodeError:
                 dil.delete()
-                if path.isfile("./userbot/language/DEFAULT.sirijson"):
+                if path.isfile("./userbot/language/DEFAULT.miajson"):
                     LOGS.warn("Varsayılan dil dosyası kullanılıyor...")
-                    LANGUAGE_JSON = loads(open(f"./userbot/language/DEFAULT.sirijson", "r").read())
+                    LANGUAGE_JSON = loads(open(f"./userbot/language/DEFAULT.miajson", "r").read())
                 else:
                     raise Exception("Your language file is invalid")
         break
 
 if LANGUAGE_JSON == None:
-    if path.isfile(f"./userbot/language/{LANGUAGE}.sirijson"):
+    if path.isfile(f"./userbot/language/{LANGUAGE}.miajson"):
         try:
-            LANGUAGE_JSON = loads(open(f"./userbot/language/{LANGUAGE}.sirijson", "r").read())
+            LANGUAGE_JSON = loads(open(f"./userbot/language/{LANGUAGE}.miajson", "r").read())
         except JSONDecodeError:
             raise Exception("Invalid json file")
     else:
-        if path.isfile("./userbot/language/DEFAULT.sirijson"):
+        if path.isfile("./userbot/language/DEFAULT.miajson"):
             LOGS.warn("Varsayılan dil dosyası kullanılıyor...")
-            LANGUAGE_JSON = loads(open(f"./userbot/language/DEFAULT.sirijson", "r").read())
+            LANGUAGE_JSON = loads(open(f"./userbot/language/DEFAULT.miajson", "r").read())
         else:
             raise Exception(f"Didn't find {LANGUAGE} file")
 
